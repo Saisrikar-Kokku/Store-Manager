fbe967129db1ed40a3f1276462d110b1d8ec26bc
Saisrikar-Kokku <saisrikarkokku7674@gmail.com>
2025-06-25 18:43:20 +0530

    Initial commit

--- src/components/UserSync.tsx
+++ src/components/UserSync.tsx
@@ -1,17 +1,15 @@
 "use client";
 import { useEffect } from 'react';
 import { useUser } from '@clerk/nextjs';
-import { useBusiness } from '../context/TransactionContext';
 
 const UserSync: React.FC = () => {
   const { user } = useUser();
-  const { setUserId } = useBusiness();
 
   useEffect(() => {
     if (user?.id) {
-      setUserId(user.id);
+      // setUserId(user.id);
     }
-  }, [user, setUserId]);
+  }, [user]);
 
   return null;
 };
